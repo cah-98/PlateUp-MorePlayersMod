5b10ccd92300dc03302aef063bbdeee9ec8216fe
carl <carl@cah98.org>
2022-11-11 18:20:58 +0100

    Resolves #1

--- MorePlayers/Config/ConfigHelper.cs
+++ MorePlayers/Config/ConfigHelper.cs
@@ -11,14 +11,21 @@ namespace MorePlayers.Config;
 public static class ConfigHelper
 {
     private static ConfigEntry<int> _maxPlayers;
+    private static ConfigEntry<float> _neededPlayerConfirmation;
 
     public static void SetUp(ConfigFile config)
     {
         _maxPlayers = config.Bind("Players", "Max players", 4, "How many players should be able to join your lobbies.");
+        _neededPlayerConfirmation = config.Bind("Players", "Player confirmation count", 1.0f, "How many players that need to confirm a vote to move on.");
     }
 
     public static int getMaxPlayers()
     {
         return _maxPlayers.Value;
+    }
+
+    public static float getNeededPlayerConfirmation()
+    {
+        return _neededPlayerConfirmation.Value;
     }
 }
---
+++ MorePlayers/MorePlayers/ConsentElementOverride.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Kitchen.NetworkSupport;
+using Steamworks;
+using Steamworks.Data;
+using Kitchen;
+using Controllers;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Users;
+using UnityEngine.InputSystem.LowLevel;
+using KitchenData;
+using Kitchen.Modules;
+using MorePlayers.Config;
+using System.Diagnostics;
+using Shapes;
+using UnityEngine;
+
+namespace MorePlayers
+{
+    [HarmonyPatch(typeof(ConsentElement), "Update")]
+    public class ConsentElementOverridePatch_Update
+    {
+        [HarmonyPrefix]
+        public static bool Prefix(ConsentElement __instance)
+        {
+            Traverse CE = Traverse.Create(__instance);
+            Traverse Progress = CE.Field("Progress");
+
+            float progressSpeed = 0f;
+
+            Dictionary<int, bool> consents = CE.Field("Consents").GetValue<Dictionary<int, bool>>();
+            int NumConsents = 0;
+            foreach (KeyValuePair<int, bool> consent in consents)
+            {
+                if (consent.Value)
+                {
+                    NumConsents++;
+                }
+            }
+            //UnityEngine.Debug.Log(NumConsents);
+            if ((float)NumConsents / consents.Count >= ConfigHelper.getNeededPlayerConfirmation())
+            {
+                progressSpeed = (float)NumConsents / consents.Count;
+            }
+            //UnityEngine.Debug.Log(progressSpeed);
+            
+            //CE.Method("GetProgressSpeed").GetValue<float>();
+            bool flag = progressSpeed <= 0f;
+            if (flag)
+            {
+                Progress.SetValue(Progress.GetValue<float>() - 2f * Time.unscaledDeltaTime);
+            }
+            else
+            {
+                Progress.SetValue(Progress.GetValue<float>() + progressSpeed * Time.unscaledDeltaTime);
+            }
+            CE.Property("IsCompleted").SetValue(Progress.GetValue<float>() >= 1f);
+            Progress.SetValue(Mathf.Clamp01(Progress.GetValue<float>()));
+            CE.Method("UpdateBar").GetValue();
+
+            return false;
+        }
+    }
+}
